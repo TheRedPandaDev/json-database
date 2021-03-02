f49134c5eaec4984465c5bb55a79aec84cc06a96
RealThomasMiles <dmilya2000@gmail.com>
2021-03-02 22:44:44 +0300

    Added tests

--- test/JsonDatabaseTest.java
+++ test/JsonDatabaseTest.java
@@ -5,60 +5,165 @@ import org.hyperskill.hstest.testing.TestedProgram;
 
 public class JsonDatabaseTest extends StageTest<String> {
 
-    private static final String CORRECT_SERVER_OUTPUT =
-        "Server started!\n" +
-            "Received: Give me a record # N\n" +
-            "Sent: A record # N was sent!";
+    private static final String OK_STATUS = "OK";
+    private static final String ERROR_STATUS = "ERROR";
 
-    private static final String CORRECT_CLIENT_OUTPUT =
-        "Client started!\n" +
-            "Sent: Give me a record # N\n" +
-            "Received: A record # N was sent!";
+    private static final String WRONG_EXIT = "The server should stop when client sends 'exit' request";
+    private static final String WRONG_GET_EMPTY_CELL_WITH_ERROR = "When a client tries to get an empty cell from " +
+            "the server, the server should response with 'ERROR' and the client should print that response";
+    private static final String WRONG_SET_VALUE_TO_CELL_WITH_OK = "When a client tries to save a value on" +
+            " the server, the server should save the value and response with '" + OK_STATUS + "'. The client should " +
+            "print that response";
+    private static final String WRONG_GET_VALUE = "When a client tries to get a not empty cell from the server, " +
+            "the server should response with a value of the cell. The client should pint received value.\n" +
+            "May be the problem is in processing 'set' action:\nIf the specified cell already contains " +
+            "information, you should simply rewrite it.";
+    private static final String WRONG_DELETE = "When a client tries to delete a value from the cell on the server," +
+            " the server should assign an empty string to this cell and response with '" + OK_STATUS + "'.";
+    private static final String WRONG_DELETE_EMPTY = "When a client tries to delete a cell with an empty value from the server," +
+            " the server should assign an empty string to this cell and response with '" + OK_STATUS + "'.";
+    private static final String WRONG_DELETE_INDEX_OUT_OF_BOUNDS = "When a user tries to delete a cell which index " +
+            "is less than 0 or greater than 1000, the server should response with  '" + ERROR_STATUS + "'.";
 
     @DynamicTestingMethod
-    CheckResult test() throws InterruptedException {
+    CheckResult checkExit() {
 
-        TestedProgram server = new TestedProgram("server");
+        TestedProgram server = getServer();
         server.startInBackground();
-        Thread.sleep(500);
 
-        String serverOutput = server.getOutput().trim();
+        TestedProgram client = getClient();
+        client.start("-t", "exit");
 
-        if (!serverOutput.trim().equals("Server started!")) {
-            return CheckResult.wrong("Server output should be 'Server started!' until a client connects!");
+        try {
+            Thread.sleep(50);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
 
-        TestedProgram client = new TestedProgram("client");
-
-        String clientOutput = client.start();
-        serverOutput += "\n" + server.getOutput();
-
-        String[] serverOutputLines = serverOutput.split("\n");
-
-        if (serverOutputLines.length != 3) {
-            return CheckResult.wrong("After the client connects to the server, the server output should contain 3 lines!");
-        }
-
-        String serverOutputLastLine = serverOutputLines[serverOutputLines.length - 1];
-
-        if (!serverOutputLastLine.contains("Sent: A record #") || !serverOutputLastLine.contains("was sent!")) {
-            return CheckResult.wrong("Server output after client connects to the server should be:\n"
-                + CORRECT_SERVER_OUTPUT + "\n\nWhere N is some number.\n\nYour output:\n" + serverOutput);
-        }
-
-        String[] clientOutputLines = clientOutput.split("\n");
-
-        if (clientOutputLines.length != 3) {
-            return CheckResult.wrong("After the client connects to the server, the client output should contain 3 lines!");
-        }
-
-        String clientOutputLastLine = clientOutputLines[clientOutputLines.length - 1];
-
-        if (!clientOutputLastLine.contains("Received: A record #") || !clientOutputLastLine.contains("was sent!")) {
-            return CheckResult.wrong("Client output after client connects to the server should be:\n"
-                + CORRECT_CLIENT_OUTPUT + "\n\nWhere N is some number.\n\nYour output:\n" + clientOutput);
+        if (!server.isFinished()) {
+            server.stop();
+            return CheckResult.wrong(WRONG_EXIT);
         }
 
         return CheckResult.correct();
+    }
+
+    @DynamicTestingMethod
+    CheckResult testInputs() {
+
+        TestedProgram server = getServer();
+        server.startInBackground();
+
+        TestedProgram client;
+        String output;
+        String expectedValue;
+
+        client = getClient();
+        output = client.start("-t", "get", "-i", "1");
+        if (!output.toUpperCase().contains(ERROR_STATUS)) {
+            return CheckResult.wrong(WRONG_GET_EMPTY_CELL_WITH_ERROR);
+        }
+
+        client = getClient();
+        output = client.start("-t", "set", "-i", "1", "-m", "Hello world!");
+        if (!output.toUpperCase().contains(OK_STATUS)) {
+            return CheckResult.wrong(WRONG_SET_VALUE_TO_CELL_WITH_OK);
+        }
+
+        client = getClient();
+        output = client.start("-t", "set", "-i", "1", "-m", "HelloWorld!");
+        if (!output.toUpperCase().contains(OK_STATUS)) {
+            return CheckResult.wrong(WRONG_SET_VALUE_TO_CELL_WITH_OK);
+        }
+
+        client = getClient();
+        output = client.start("-t", "get", "-i", "1");
+        expectedValue = "HelloWorld!";
+        if (!output.contains(expectedValue)) {
+            return CheckResult.wrong(WRONG_GET_VALUE +
+                    "\nExpected:\n" + expectedValue + "\nYour output:\n" + output);
+        }
+
+        client = getClient();
+        output = client.start("-t", "delete", "-i", "1");
+        if (!output.toUpperCase().contains(OK_STATUS)) {
+            return CheckResult.wrong(WRONG_DELETE);
+        }
+
+        client = getClient();
+        output = client.start("-t", "delete", "-i", "1");
+        if (!output.toUpperCase().contains(OK_STATUS)) {
+            return CheckResult.wrong(WRONG_DELETE_EMPTY);
+        }
+
+        client = getClient();
+        output = client.start("-t", "get", "-i", "1");
+        if (!output.toUpperCase().contains(ERROR_STATUS)) {
+            return CheckResult.wrong(WRONG_GET_EMPTY_CELL_WITH_ERROR + "\nMay be after deleting a cell you didn't " +
+                    "assign an empty value to it.");
+        }
+
+        client = getClient();
+        output = client.start("-t", "set", "-i", "55", "-m", "Some text here");
+        if (!output.toUpperCase().contains(OK_STATUS)) {
+            return CheckResult.wrong(WRONG_SET_VALUE_TO_CELL_WITH_OK);
+        }
+
+        client = getClient();
+        output = client.start("-t", "get", "-i", "55");
+        expectedValue = "Some text here";
+        if (!output.contains(expectedValue)) {
+            return CheckResult.wrong(WRONG_GET_VALUE +
+                    "\nExpected:\n" + expectedValue + "\nYour output:\n" + output);
+        }
+
+        client = getClient();
+        output = client.start("-t", "get", "-i", "56");
+        if (!output.toUpperCase().contains(ERROR_STATUS)) {
+            return CheckResult.wrong(WRONG_GET_EMPTY_CELL_WITH_ERROR);
+        }
+
+        client = getClient();
+        output = client.start("-t", "delete", "-i", "55");
+        if (!output.toUpperCase().contains(OK_STATUS)) {
+            return CheckResult.wrong(WRONG_DELETE);
+        }
+
+        client = getClient();
+        output = client.start("-t", "delete", "-i", "56");
+        if (!output.toUpperCase().contains(OK_STATUS)) {
+            return CheckResult.wrong(WRONG_DELETE_EMPTY);
+        }
+
+        client = getClient();
+        output = client.start("-t", "delete", "-i", "100");
+        if (!output.toUpperCase().contains(OK_STATUS)) {
+            return CheckResult.wrong(WRONG_DELETE_EMPTY);
+        }
+
+        client = getClient();
+        output = client.start("-t", "delete", "-i", "1001");
+        if (!output.toUpperCase().contains(ERROR_STATUS)) {
+            return CheckResult.wrong(WRONG_DELETE_INDEX_OUT_OF_BOUNDS);
+        }
+
+        client = getClient();
+        client.start("-t", "exit");
+
+        return CheckResult.correct();
+    }
+
+
+    private static TestedProgram getClient() {
+        return new TestedProgram("client");
+    }
+
+    private static TestedProgram getServer() {
+        return new TestedProgram("server");
+    }
+
+    private static void stopServer() {
+        TestedProgram client = getClient();
+        client.start("-t", "exit");
     }
 }
