03184a0a245d519fce4330274d867c0eb1f51650
RealThomasMiles <dmilya2000@gmail.com>
2021-03-05 20:58:23 +0300

    Added ExecutorService

--- src/server/Main.java
+++ src/server/Main.java
@@ -8,40 +8,48 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
 
     private static final int PORT = 34522;
-    private static final Database database = new Database();
+    private static final String pathToFile = "./src/server/data/";
+    private static final Database database = new Database(pathToFile);
     private static final Controller controller = new Controller();
     private static final WorkingToggle workingToggle = new WorkingToggle();
 
     public static void main(String[] args) {
+        ExecutorService executorService = Executors
+                .newFixedThreadPool(Runtime.getRuntime()
+                        .availableProcessors());
         try (ServerSocket serverSocket = new ServerSocket(PORT)) {
             System.out.println("Server started!");
             while (workingToggle.isWorking()) {
-                Session session = new Session(serverSocket.accept());
-                session.start();
-                session.join(); // Temporary measure for exiting
+                Session session = new Session(serverSocket.accept(), serverSocket);
+                executorService.submit(session);
             }
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
+        } catch (IOException ignored) {
+        } finally {
+            executorService.shutdown();
         }
     }
 
-    static class Session extends Thread {
+    static class Session implements Runnable {
         private final Socket socket;
+        private final ServerSocket serverSocket;
 
-        public Session(Socket socketForClient) {
+        public Session(Socket socketForClient, ServerSocket serverSocket) {
             super();
             this.socket = socketForClient;
+            this.serverSocket = serverSocket;
         }
 
         @Override
         public void run() {
             try (
                     DataInputStream inputStream = new DataInputStream(socket.getInputStream());
-                    DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
+                    DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream())
             ) {
                 String receivedMsg = inputStream.readUTF();
                 System.out.println("Received: " + receivedMsg);
@@ -65,7 +73,7 @@ public class Main {
 
         public Command parseInput(CommandArgs commandArgs) {
             if ("exit".equals(commandArgs.getType())) {
-                return new ExitCommand(workingToggle);
+                return new ExitCommand(workingToggle, serverSocket);
             } else if (("set".equals(commandArgs.getType()))) {
                 return new SetCommand(database, commandArgs.getKey(), commandArgs.getValue());
             } else if (("get".equals(commandArgs.getType()))) {
--- src/server/commands/ExitCommand.java
+++ src/server/commands/ExitCommand.java
@@ -2,17 +2,27 @@ package server.commands;
 
 import server.WorkingToggle;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class ExitCommand implements Command {
     WorkingToggle workingToggle;
+    ServerSocket serverSocket;
     Result result;
 
-    public ExitCommand(WorkingToggle workingToggle) {
+    public ExitCommand(WorkingToggle workingToggle, ServerSocket serverSocket) {
         this.workingToggle = workingToggle;
+        this.serverSocket = serverSocket;
     }
 
     @Override
     public void execute() {
         workingToggle.stopWorking();
+        try {
+            serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         result = new Result("OK");
     }
 
